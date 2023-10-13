028574a8f1d6d6d8cc00097bebc602c69bedbb94
uofa-hjohns <101161870+uofa-hjohns@users.noreply.github.com>
2023-10-13 06:48:26 +0000

    Configuring mysql permissions

--- mysql_init.py
+++ mysql_init.py
@@ -2,3 +2,7 @@ import os
 os.system("sudo apt-get update")
 os.system("sudo apt-get -y install mysql-server")
 os.system("python -m pip install mysql-connector-python")
+#os.system("sudo /etc/init.d/mysql stop")
+#os.system("sudo service mysql stop")
+#os.system("sudo killall -KILL mysql mysqld_safe mysqld")
+#os.system("sudo mysqld_safe")
