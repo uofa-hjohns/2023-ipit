f775578a270e1e3aff922e7ec133424381cd1900
uofa-hjohns <101161870+uofa-hjohns@users.noreply.github.com>
2023-10-13 08:39:31 +0000

    More and more testing

--- mysql_db.py
+++ mysql_db.py
@@ -1,8 +1,9 @@
-import mysql.connector
+import os
+os.system("sudo mysql < mysql_root")
 
+import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root"
 )
-
 print(mydb) 
--- mysql_init.sh
+++ mysql_init.sh
@@ -6,3 +6,4 @@ sudo /etc/init.d/mysql stop
 sudo service mysql stop
 sudo killall -KILL mysql mysqld_safe mysqld
 sudo mysqld_safe &
+echo "Please open "mysql_db.py" in a new terminal in roughly 1 min"
