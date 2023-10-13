8f7f7e246e0387675ee137ddaa346ca8ac8b1c28
uofa-hjohns <101161870+uofa-hjohns@users.noreply.github.com>
2023-10-13 09:29:44 +0000

    Use subprocess to init

--- mysql_init.py
+++ mysql_init.py
@@ -1,9 +1,30 @@
 import os
-os.system("sudo apt-get update")
-os.system("sudo apt-get -y install mysql-server")
-os.system("python -m pip install mysql-connector-python")
-os.system("sudo mysql < mysql_root")
-os.system("sudo /etc/init.d/mysql stop")
-os.system("sudo service mysql stop")
-os.system("sudo killall -KILL mysql mysqld_safe mysqld")
-os.system("sudo mysqld_safe &")
+import subprocess
+import time
+
+subprocess.run("sudo apt-get update", shell=True, stdout=subprocess.DEVNULL)
+print("APT updated")
+
+subprocess.run("sudo apt-get -y install mysql-server", shell=True, stdout=subprocess.DEVNULL)
+print("MySQL installed through APT")
+
+subprocess.run("python -m pip install mysql-connector-python", shell=True, stdout=subprocess.DEVNULL)
+print("MySQL Connector Python installed through Python")
+
+subprocess.run("sudo /etc/init.d/mysql stop", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+subprocess.run("sudo service mysql stop", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+subprocess.run("sudo killall -KILL mysql mysqld_safe mysqld", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+print("All MySQL instances killed")
+
+subprocess.run("sudo mysqld_safe &", shell=True, stdout=subprocess.DEVNULL)
+print("MySQL server started in safe mode")
+
+print("Waiting 15 seconds before updating MySQL default database")
+time.sleep(5)
+print("Waiting 10 seconds before updating MySQL default database")
+time.sleep(5)
+print("Waiting 5 seconds before updating MySQL default database")
+time.sleep(5)
+
+subprocess.run("sudo mysql < mysql_root", shell=True)
+print("Updated default MySQL database")
