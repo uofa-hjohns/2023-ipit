af92c2a05821bb6cca71989cb605010cc39d7dd7
uofa-hjohns <101161870+uofa-hjohns@users.noreply.github.com>
2023-10-17 05:07:46 +0000

    implemented all create tables, broken insert func

--- server/mysql/mysql_db.py
+++ server/mysql/mysql_db.py
@@ -2,6 +2,7 @@ import sys
 import mysql.connector
 import os # for dumping
 import subprocess # for dumping
+import csv # for ledger
 
 first_time = 0
 
@@ -52,13 +53,88 @@ def db_IPITimport():
 
 # BACKUP/DUMP IPIT DATABASE TO FILE
 def db_IPITexport():
-  subprocess.run("sudo mysqldump --databases ipit > ipit.sql", shell=True, stdout=subprocess.DEVNULL)
+  subprocess.run("sudo mysqldump --databases ipit > ipit.sql", shell=True)
 
 # CREATE IPIT DATABASE
 def db_IPITcreate(db,dbcr):
-  dbcr.execute("CREATE TABLE Customers (CustomerID VARCHAR(255) PRIMARY KEY, CustomerName VARCHAR(255), BillerID VARCHAR(255))")
-  dbcr.execute("CREATE TABLE Vendors (VendorID INT NOT NULL PRIMARY KEY, VendorName VARCHAR(255))")
-  dbcr.execute("CREATE TABLE Transactions (InvoiceID VARCHAR(255) PRIMARY KEY, Amount INT NOT NULL, CustomerID VARCHAR(255), VendorID INT NOT NULL, CONSTRAINT FK_TransCust FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID), CONSTRAINT FK_TransVend FOREIGN KEY (VendorID) REFERENCES Vendors(VendorID))")
+  SQLExec = [
+  """CREATE TABLE Customers (
+  Customer_ID VARCHAR(255) PRIMARY KEY, 
+  Customer_Name VARCHAR(255), 
+  Customer_BillerID VARCHAR(255)
+  )""",
+
+  """CREATE TABLE Vendors (
+  Vendor_ID INT NOT NULL PRIMARY KEY, 
+  Vendor_Name VARCHAR(255)
+  )""",
+  
+  """CREATE TABLE Transactions (
+  Transaction_InvoiceID VARCHAR(255) PRIMARY KEY, 
+  Transaction_InvoiceAmount INT NOT NULL, 
+  TransactionFK_Customer_ID VARCHAR(255), 
+  TransactionFK_Vendor_ID INT NOT NULL, 
+  CONSTRAINT FK_TransactionCustomers FOREIGN KEY (TransactionFK_Customer_ID) REFERENCES Customers(Customer_ID), 
+  CONSTRAINT FK_TransactionVendors FOREIGN KEY (TransactionFK_Vendor_ID) REFERENCES Vendors(Vendor_ID)
+  )""",
+  
+  """CREATE TABLE GeneralLedgers (
+  GL_AccountID INT NOT NULL PRIMARY KEY, 
+  GL_AccountDescription VARCHAR(255)
+  )""",
+  
+  """CREATE TABLE Operators (
+  Operator_ID VARCHAR(255) PRIMARY KEY, 
+  Operator_Name VARCHAR(255)
+  )""",
+  
+  """CREATE TABLE Sources (
+  Source_ID VARCHAR(255) PRIMARY KEY, 
+  Source_LongDescription VARCHAR(255)
+  )""",
+  
+  """CREATE TABLE GeneralLedgerEntries (
+  GLE_LineNumber INT NOT NULL PRIMARY KEY, 
+  GLE_LineDescription VARCHAR(255), 
+  GLE_Period INT NOT NULL, 
+  GLEFK_GL_AccountID INT NOT NULL, 
+  GLEFK_Operator_ID VARCHAR(255), 
+  GLEFK_Source_ID VARCHAR(255), 
+  CONSTRAINT FK_GLEtGL FOREIGN KEY (GLEFK_GL_AccountID) REFERENCES GeneralLedgers(GL_AccountID), 
+  CONSTRAINT FK_GLEOper FOREIGN KEY (GLEFK_Operator_ID) REFERENCES Operators(Operator_ID), 
+  CONSTRAINT FK_GLESrc FOREIGN KEY (GLEFK_Source_ID) REFERENCES Sources(Source_ID)
+  )""",
+  
+  """CREATE TABLE Journals (
+  Journal_ID VARCHAR(255) PRIMARY KEY,
+  Journal_Date DATE,
+  Journal_EntryDate DATE,
+  JournalFK_GLE_LineNumber INT NOT NULL,
+  JournalFK_Transaction_InvoiceID VARCHAR(255),
+  CONSTRAINT FK_Journals_GLE FOREIGN KEY (JournalFK_GLE_LineNumber) REFERENCES GeneralLedgerEntries(GLE_LineNumber),
+  CONSTRAINT FK_Journals_Transactions FOREIGN KEY (JournalFK_Transaction_InvoiceID) REFERENCES Transactions(Transaction_InvoiceID)
+  )"""]
+  
+  for x in SQLExec:
+    dbcr.execute(x)
+
+
+def getCSVarray():
+  ledgerarray = []
+  with open('Operating Expense Transactions.csv', 'r') as file:
+    LedgerCSV = csv.reader(file)
+    for entry in LedgerCSV:
+        if entry[0] != "Journal ID":
+          ledgerarray.append(entry)
+  return ledgerarray
+
+def InsertIntoSQL(array,db,dbcr):
+  for entry in array:
+    Customer_ID = entry[17]
+    Customer_Name = entry[18]
+    Customer_BillerID = entry[19]
+    dbcr.execute(f"""INSERT IGNORE INTO Customers (Customer_ID, Customer_Name, Customer_BillerID) VALUES ('{Customer_ID}', '{Customer_Name}', '{Customer_BillerID}')""")
+    print("done1")
 
 
 # MAIN FUNCTION
@@ -70,6 +146,7 @@ def main():
     print("IPIT database is empty, initializing...")
     db_IPITcreate(db,dbcr)
 
+  #InsertIntoSQL(getCSVarray(),db,dbcr)
   db_IPITexport()
 
 
